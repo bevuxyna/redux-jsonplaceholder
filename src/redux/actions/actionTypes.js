1218af9fb41f3c7b203715d05c73913ba853d4ab
Vlada Melnikova <wannhorn@gmail.com>
2023-06-03 15:38:50 +0300

    feat: add fetch action

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,3 @@
+export const GET_USERS_FETCH = 'GET_USERS_FETCH';
+export const GET_USERS_ERROR = 'GET_USERS_ERROR';
+export const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS';
---
+++ src/redux/asyncAction.js
@@ -0,0 +1,13 @@
+import {getUsersFailure, getUsersFetch, getUsersSuccess} from "./actions/users";
+
+export const fetchUsers = (dispatch) => {
+    return function () {
+        dispatch(getUsersFetch());
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(json => {
+                dispatch(getUsersSuccess(json));
+            })
+            .catch((error) => dispatch(getUsersFailure(error)))
+    }
+}
