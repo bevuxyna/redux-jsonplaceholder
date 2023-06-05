3b79f81ee2df8fe685d986cb5defc5696d672556
Vlada Melnikova <wannhorn@gmail.com>
2023-06-05 20:42:10 +0300

    fix: refactor styles

--- src/Components/InfoItem/InfoItem.jsx
+++ src/Components/InfoItem/InfoItem.jsx
@@ -3,7 +3,7 @@ import './InfoItem.scss';
 
 function InfoItem({ image, altText, text }) {
     return (
-        <div className='info-item__container'>
+        <div className='info-item'>
             <img src={image} className='info-item__image' alt={altText} />
             <p className='info-item__text'>{text}</p>
         </div>
